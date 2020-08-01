f214c0ad02c882df4608be85c2531d9f997d4364
Amanda Brito <amanda.brito1302@gmail.com>
2020-08-01 12:04:49 -0300

    Alterações para a página ficar personalizada
    Foram colocados os meus vídeos e a página foi atualizada para ter os elementos visuais

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import Menu from './componentes/Menu'
+import dadosIniciais from './data/dados_iniciais.json';
+import BannerMain from './componentes/BannerMain';
+import Carousel from './componentes/Carousel';
+import Footer from './componentes/Footer';
 
 function App() {
   return (
-    <div >
+    <div style={{background:"#141414"}}>
       <Menu />
+
+      <BannerMain 
+      videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+      videoDescription={"Versão de Xbox 360 e PS3"}
+      url={dadosIniciais.categorias[0].videos[0].url}/>
+
+      <Carousel
+      ignoreFirstVideo
+      category={dadosIniciais.categorias[0]} />
+
+      <Carousel category={dadosIniciais.categorias[1]} />
+      
+      <Footer/>
     </div>
+
   );
 }
 
