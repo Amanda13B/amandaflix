19d0e8ca26ec4864fa10921c881f320817463eb2
Amanda Brito <amanda.brito1302@gmail.com>
2020-08-03 13:09:18 -0300

    Update final da quarta aula

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -27,8 +27,9 @@ function CadastroCategoria ()
 
   useEffect( ()=>
     {
-      console.log("Olá");
-      const URL = "http://localhost:8080/categorias";
+      const URL = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias'
+      : "https://amandaflix.herokuapp.com/categorias";
       fetch(URL)
         .then(async (respostaDoServidor) =>
           {
